ee6f213177987b5875105bf85414c75011d1a5d6
paladinbaby <308257336@qq.com>
2020-03-17 17:52:20 +0800

    my first Rust project for test

--- first_project/src/main.rs
+++ first_project/src/main.rs
@@ -1,3 +1,13 @@
+mod math;
+use math::{add};
+
 fn main() {
-    println!("Hello, world!");
+    let input1 = "my_first";
+    println!("input1 is {}", input1);
+    let input2 = String::from("hello");
+    println!("input2 is {}", input2);
+    let rand_number = rand::random();
+    println!("You {}!", if rand_number { "win" } else { "lose" });
+    let result = add(1, 2);
+    println!("1 + 2 = {}", result);
 }
